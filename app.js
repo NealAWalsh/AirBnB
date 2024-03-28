590871a3b206a317cc19f289aadef9c87406ec14
Neal Walsh <156786374+NealAWalsh@users.noreply.github.com>
2024-03-28 12:12:04 +0000

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const url = "https://public.tableau.com/views/AirBnB-Boston-2016-01/Dashboard1?:
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
-    height: 1000,
-    width: 1400,
+    height: 100%,
+    width: 100%,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
